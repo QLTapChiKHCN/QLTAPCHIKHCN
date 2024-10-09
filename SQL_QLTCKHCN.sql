839e227a9b434afbeb0f0dd024c8fcbba7f54d6b
Thiên Đức <105533589+DevS1801@users.noreply.github.com>
2024-10-09 20:20:25 +0700

    SQL_QLTCKHCN

---
+++ SQL_QLTCKHCN.sql
@@ -0,0 +1,174 @@
+﻿-- Tạo CSDL
+CREATE DATABASE QL_TCKHCN;
+GO
+
+USE QL_TCKHCN;
+GO
+
+-- Tạo bảng NgonNgu
+CREATE TABLE NgonNgu (
+    MaNgonNgu varchar(10) PRIMARY KEY,
+    TenNgonNgu nvarchar(50)
+);
+GO	
+-- Tạo bảng ChuyenMuc
+CREATE TABLE ChuyenMuc (
+    MaChuyenMuc varchar(10) PRIMARY KEY,
+    TenChuyenMuc nvarchar(100)
+);
+GO
+-- Tạo bảng SoTapChi
+CREATE TABLE SoTapChi (
+    MaSoTC varchar(10) PRIMARY KEY,
+    TenSo nvarchar(100),
+    AnhBia varchar(200),
+    NgayXuatBan date
+);
+GO
+-- Tạo bảng HocVi
+CREATE TABLE HocVi (
+    MaHocVi varchar(10) PRIMARY KEY,
+    TenHocVi nvarchar(50)
+);
+GO
+-- Tạo bảng HocHam
+CREATE TABLE HocHam (
+    MaHocHam varchar(10) PRIMARY KEY,
+    TenHocHam nvarchar(50)
+);
+GO
+-- Tạo bảng VaiTro
+CREATE TABLE VaiTro (
+    MaVaiTro varchar(10) PRIMARY KEY,
+    TenVaiTro nvarchar(50)
+);
+GO
+-- Tạo bảng LoaiTacGia
+CREATE TABLE LoaiTacGia (
+    MaLTacGia varchar(10) PRIMARY KEY,
+    TenLoai nvarchar(50)
+);
+GO
+-- Tạo bảng NguoiDung
+CREATE TABLE NguoiDung (
+    MaNguoiDung varchar(10) PRIMARY KEY,
+    MaHocVi varchar(10) FOREIGN KEY REFERENCES HocVi(MaHocVi),
+    MaHocHam varchar(10) FOREIGN KEY REFERENCES HocHam(MaHocHam),
+    TenDangNhap varchar(50) NOT NULL,
+    MatKhau varchar(100),
+    HoTen nvarchar(100),
+    Email varchar(100),
+    CCCD varchar(12),
+    SoDienThoai varchar(15),
+    DiaChi nvarchar(200),
+    ChuyenNganh nvarchar(100),
+    DonVi nvarchar(200),
+    QuocGia nvarchar(50),
+    GioiTinh nvarchar(10)
+);
+GO
+-- Tạo bảng NguoiDung_VaiTro
+CREATE TABLE NguoiDung_VaiTro (
+    MaNguoiDung varchar(10),
+    MaVaiTro varchar(10),
+    PRIMARY KEY (MaNguoiDung, MaVaiTro),
+    FOREIGN KEY (MaNguoiDung) REFERENCES NguoiDung(MaNguoiDung),
+    FOREIGN KEY (MaVaiTro) REFERENCES VaiTro(MaVaiTro)
+);
+GO
+-- Tạo bảng BaiViet
+CREATE TABLE BaiViet (
+    MaBaiBao char(10) PRIMARY KEY,
+    MaNgonNgu varchar(10) FOREIGN KEY REFERENCES NgonNgu(MaNgonNgu) NOT NULL,
+    MaSoTC varchar(10) FOREIGN KEY REFERENCES SoTapChi(MaSoTC),
+    MaChuyenMuc varchar(10) FOREIGN KEY REFERENCES ChuyenMuc(MaChuyenMuc),
+    TieuDe nvarchar(200),
+    TenBaiBao varchar(100),
+    NgayXetDuyet date,
+    NgayGui date,
+    TuKhoa nvarchar(200),
+    FileBaiViet VARBINARY(MAX), -- Sử dụng VARBINARY(MAX) để lưu trữ file
+    TrangThai nvarchar(50)
+);
+-- Tạo bảng ChiTietBaiViet
+CREATE TABLE ChiTietBaiViet (
+    MaBaiBao char(10),
+    MaNguoiDung varchar(10),
+    MaLTacGia varchar(10),
+    PRIMARY KEY (MaBaiBao, MaNguoiDung, MaLTacGia),
+    FOREIGN KEY (MaBaiBao) REFERENCES BaiViet(MaBaiBao),
+    FOREIGN KEY (MaNguoiDung) REFERENCES NguoiDung(MaNguoiDung),
+    FOREIGN KEY (MaLTacGia) REFERENCES LoaiTacGia(MaLTacGia)
+);
+GO
+-- Tạo bảng ChiTietPhanBien
+CREATE TABLE ChiTietPhanBien (
+    MaBaiBao char(10),
+    MaNguoiDung varchar(10),
+    KetQuaPhanBien nvarchar(50),
+    YKienPhanBien ntext,
+    NgayPhanBien date,
+    FilePhanBien VARBINARY(MAX), -- Lưu trữ file phản biện dưới dạng dữ liệu nhị phân
+    PRIMARY KEY (MaBaiBao, MaNguoiDung),
+    FOREIGN KEY (MaBaiBao) REFERENCES BaiViet(MaBaiBao),
+    FOREIGN KEY (MaNguoiDung) REFERENCES NguoiDung(MaNguoiDung)
+);
+GO
+-- Tạo bảng LichSuChonNguoiPhanBien
+CREATE TABLE LichSuChonNguoiPhanBien (
+    MaNguoiDung varchar(10),
+    MaBaiBao char(10),
+    TrangThai nvarchar(50),
+    PRIMARY KEY (MaNguoiDung, MaBaiBao),
+    FOREIGN KEY (MaNguoiDung) REFERENCES NguoiDung(MaNguoiDung),
+    FOREIGN KEY (MaBaiBao) REFERENCES BaiViet(MaBaiBao)
+);
+GO
+
+---Insert dữ liệu
+INSERT INTO NgonNgu (MaNgonNgu, TenNgonNgu) VALUES
+('NN01', N'Tiếng Việt'),
+('NN02', N'English')
+go
+INSERT INTO ChuyenMuc (MaChuyenMuc, TenChuyenMuc) VALUES
+('CM01', N'KHOA HỌC - CÔNG NGHỆ'),
+('CM02', N'QUẢN LÝ KINH TẾ')
+INSERT INTO HocVi (MaHocVi, TenHocVi) VALUES
+('HV01', N'Cử nhân'),
+('HV02', N'Kỹ sư'),
+('HV03', N'Thạc sĩ'),
+('HV04', N'Tiến sĩ')
+INSERT INTO HocHam (MaHocHam, TenHocHam) VALUES
+('HH01', N'Giáo sư'),
+('HH02', N'Phó giáo sư'),
+('HH03', N'Tiến sĩ'),
+('HH04', N'Thạc sĩ');
+
+INSERT INTO VaiTro (MaVaiTro, TenVaiTro) VALUES
+('VT01', N'Biên tập viên'),
+('VT02', N'Tổng biên tập'),
+('VT03', N'Tác giả'),
+('VT04', N'Phản biện')
+
+INSERT INTO NguoiDung (MaNguoiDung, MaHocVi, MaHocHam, TenDangNhap, MatKhau, HoTen, Email, CCCD, SoDienThoai, DiaChi, ChuyenNganh, DonVi, QuocGia, GioiTinh) VALUES
+('ND01', 'HV01', 'HH03', 'user1', 'pass1', N'Nguyễn Văn A', 'a@example.com', '123456789012', '0123456789', N'Hà Nội', N'Công nghệ thông tin', N'Trường Đại học', N'Việt Nam', N'Nam'),
+('ND02', 'HV02', 'HH01', 'user2', 'pass2', N'Trần Thị B', 'b@example.com', '123456789013', '0123456788', N'Hồ Chí Minh', N'Y học', N'Bệnh viện', N'Việt Nam', N'Nữ'),
+('ND03', 'HV03', 'HH02', 'user3', 'pass3', N'Lê Văn C', 'c@example.com', '123456789014', '0123456787', N'Đà Nẵng', N'Giáo dục', N'Trường Đại học', N'Việt Nam', N'Nam'),
+('ND04', 'HV04', 'HH03', 'user4', 'pass4', N'Phạm Thị D', 'd@example.com', '123456789015', '0123456786', N'Hải Phòng', N'Khoa học xã hội', N'Trường Cao đẳng', N'Việt Nam', N'Nữ'),
+('ND05', 'HV01', 'HH02', 'user5', 'pass5', N'Nguyễn Văn E', 'e@example.com', '123456789016', '0123456785', N'Can Tho', N'Văn hóa nghệ thuật', N'Trường Đại học', N'Việt Nam', N'Nam');
+
+INSERT INTO NguoiDung_VaiTro (MaNguoiDung, MaVaiTro) VALUES
+('ND01', 'VT01'),  -- Nguyễn Văn A là Biên tập viên
+('ND02', 'VT02'),  -- Trần Thị B là Tổng biên tập
+('ND03', 'VT03'),  -- Lê Văn C là Tác giả
+('ND03', 'VT04'),  -- Lê Văn C là Người phản biện
+('ND05', 'VT04');  -- Nguyễn Văn E là Người phản biện
+
+INSERT INTO LoaiTacGia (MaLTacGia, TenLoai) VALUES
+('LTG01', N'Tác giả chính + Tác giả liên hệ'),
+('LTG02', N'Hỗ trợ'),
+('LTG03', N'Tác giả chính'),
+('LTG04', N'Tác giả liên hệ'),
+('LTG05', N'Thành viên'),
+('LTG06', N'Thư ký');
+
